b02d526d9bfe940da0d892575b9a30dd471b8e8f
Suryavipparla <suryavipparla5058@gmail.com>
2024-06-07 20:10:45 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import whisper
+
+st.title("Speech to Text")
+
+
+audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+
+model = whisper.load_model("base")
+
+
+
+if st.sidebar.button("Transcribe Audio"):
+    if audio_file is not None:
+        st.text("Text is :")
+        st.sidebar.success("Transcribing Audio")
+        transcription = model.transcribe(audio_file.name)
+        st.sidebar.success("Transcription Complete")
+        st.success(transcription["text"])
+       
+    else:
+        st.sidebar.error("Please upload an audio file")
+
+
+st.sidebar.header("Play Original Audio File")
+st.sidebar.audio(audio_file)
